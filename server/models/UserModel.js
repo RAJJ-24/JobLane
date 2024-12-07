8e3bddc533ecbe87d5e57373a29d8c95cf1234e0
RAJJ-24 <usl.spam@gmail.com>
2024-12-08 00:10:29 +0530

    Update UserModel.js

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -1,78 +1,20 @@
-const mongoose = require('mongoose')
-const validator = require('validator')
+const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user',
+  },
+});
 
-
-    name: {
-        type: String,
-        required: [true, "Please enter your name"]
-    },
-
-    email: {
-        type: String,
-        required: true,
-        validate: [validator.isEmail, "Please Enter valid email address"],
-        unique: true
-    },
-
-    password: {
-        type: String,
-        required: [true, "Please enter a password"]
-    },
-
-    avatar: {
-        public_id: {
-            type: String,
-            required: true
-        },
-        url: {
-            type: String,
-            required: true
-        },
-    },
-
-    role: {
-        type: String,
-        enum: ["applicant", "admin"],
-        default: "applicant"
-    },
-
-    skills: [
-        {
-            type: String
-        }
-    ],
-
-    resume: {
-        public_id: {
-            type: String,
-            required: false
-        },
-        url: {
-            type: String,
-            required: false
-        },
-
-    },
-    savedJobs: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Job'
-        }
-    ],
-    appliedJobs: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Application'
-        }
-    ],
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
-
-})
-
-const User = mongoose.model('User', UserSchema)
-module.exports = User
+module.exports = mongoose.model('User', UserSchema);
